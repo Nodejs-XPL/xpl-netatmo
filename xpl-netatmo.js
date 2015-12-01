da762afdfedea5c128f94c522c12c1cc53e44bef
oeuillot <olivier.oeuillot@gmail.com>
2015-12-01 14:43:55 +0100

    Fix code

--- xpl-netatmo.js
+++ xpl-netatmo.js
@@ -6,6 +6,8 @@ var commander = require('commander');
 var Netatmo = require("netatmo");
 var os = require('os');
 var util = require('util');
+var async = require('async');
+var debug = require('debug')('xpl-netatmo');
 
 commander.version(require("./package.json").version);
 commander.option("-u, --username <username>", "Netatmo username");
@@ -52,22 +54,218 @@ commander.command('start').description("Start processing Netatmo datas")
 
             var netatmo = new Netatmo(commander);
 
-            setInterval(updateDatas.bind(xpl, xpl, netatmo, devices),
-                1000 * 60 * 10);
-            updateDatas(xpl, netatmo, devices);
+            setInterval(updateDatas.bind(xpl, xpl, netatmo, devices,
+                commander.deviceAliases), 1000 * 60);
+            updateDatas(xpl, netatmo, devices, commander.deviceAliases);
+
+            xpl.on("xpl:xpl-cmnd", function(message) {
+              if (message.bodyName !== "sensor.request" ||
+                  body.command !== "request") {
+                return;
+              }
+
+              var modifs = [];
+
+              for ( var deviceKey in devices) {
+                for ( var currentKey in devices[deviceKey]) {
+                  if (/Date$/.exec(currentKey)) {
+                    continue;
+                  }
+
+                  modifs.push({
+                    device : deviceKey,
+                    type : refs[currentKey].name,
+                    current : currentValues[currentKey],
+                    date : currentValues[currentKey + "Date"].toISOString(),
+                    unit : refs[currentKey].unit
+                  });
+                }
+              }
+
+              async.eachSeries(modifs, function(body, callback) {
+                xpl.sendXplStat(body, "sensor.basic", callback);
+
+              }, function(error) {
+                if (error) {
+                  console.error(error);
+                }
+              });
+
+            });
+
           });
         });
 
-function updateDatas(xpl, netatmo, devices) {
+var refs = {
+  Temperature : {
+    name : "temp",
+    unit : "c"
+  },
+  CO2 : {
+    name : "CO2",
+    unit : "ppm"
+  },
+  Humidity : {
+    name : "humidity",
+    unit : "%"
+  },
+  Noise : {
+    name : "noise",
+    unit : "db"
+  },
+  Pressure : {
+    name : "pressure",
+    unit : "mbar"
+  },
+  AbsolutePressure : {
+    name : "absolutePressure",
+    unit : "mbar"
+  },
+  Rain : {
+    name : "rain",
+    unit : "mm"
+  },
+  WindAngle : {
+    name : "windAngle",
+    unit : "°"
+  },
+  WindStrength : {
+    name : "windStrength",
+    unit : "km/h"
+  },
+  GustAngle : {
+    name : "gustAngle",
+    unit : "°"
+  },
+  GustStrength : {
+    name : "gustStrength",
+    unit : "km/h"
+  },
+  Battery : {
+    name : "battery",
+    unit : "%"
+  }
+};
+
+function scanDevice(device, devices, modifs, aliases) {
+  var key = device._id;
+  if (aliases && aliases[key]) {
+    key = aliases[key];
+  }
+
+  var currentValues = devices[key];
+  if (!currentValues) {
+    currentValues = {};
+    devices[key] = currentValues;
+  }
+
+  var battery = device.battery_vp;
+  if (battery) {
+    var min = -1;
+    var max = 6000;
+    switch (device.type) {
+    case "NAMain":
+    case "NAModule4":
+    case "NAPlug":
+    case "NATherm1":
+      min = 4200;
+      break;
+    case "NAModule1":
+    case "NAModule3":
+      min = 3600;
+      break;
+    case "NAModule2":
+      min = 3950;
+      break;
+    }
+
+    if (min >= 0) {
+      var b = ((battery - min) / (max - min));
+
+      b = Math.floor(b * 100);
+      if (currentValues.Battery !== b) {
+        currentValues.Battery = b;
+        currentValues.BatteryDate = new Date();
+
+        modifs.push({
+          device : key,
+          type : "battery",
+          current : b,
+          date : currentValues.batteryDate.toISOString(),
+          unit : '%'
+        });
+      }
+    }
+  }
+
+  var dd = device.dashboard_data;
+  device.data_type.forEach(function(dt) {
+    if (!refs[dt]) {
+      return;
+    }
+
+    var date = new Date(dd.time_utc * 1000);
+
+    if (!dd[dt] || currentValues[dt] === dd[dt]) {
+      return;
+    }
+
+    currentValues[dt] = dd[dt];
+    currentValues[dt + "Date"] = date;
+
+    modifs.push({
+      device : key,
+      type : refs[dt].name,
+      current : currentValues[dt],
+      date : currentValues[dt + "Date"].toISOString(),
+      unit : refs[dt].unit
+    });
+  });
+
+}
+
+function updateDatas(xpl, netatmo, devices, aliases, callback) {
+  if (!callback) {
+    callback = function(error) {
+      if (error) {
+        console.error(error);
+      }
+    }
+  }
   netatmo.getStationsData(function(error, list) {
     if (error) {
       console.error(error);
       return;
     }
 
-    console.log("devices=" + util.inspect(list, {
-      depth : null
-    }));
+    if (false && debug.enabled) {
+      debug("devices=" + util.inspect(list, {
+        depth : null
+      }));
+    }
+
+    var modifs = [];
+
+    list.devices.forEach(function(device) {
+
+      scanDevice(device, devices, modifs);
+
+      if (device.modules) {
+        device.modules.forEach(function(mod) {
+          scanDevice(mod, devices, modifs, aliases);
+        });
+      }
+    });
+
+    if (!modifs.length) {
+      return callback();
+    }
+
+    debug("Send modifs", modifs);
+
+    async.eachSeries(modifs, function(body, callback) {
+      xpl.sendXplStat(body, "sensor.basic", callback);
+    }, callback);
   });
 }
 
